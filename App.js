460533959d78bf535a4aa705a8cf086bde5b8612
hung90909 <114125660+hung90909@users.noreply.github.com>
2023-10-09 23:26:52 +0700

    update_home

--- App.js
+++ App.js
@@ -9,13 +9,14 @@ import NhanTin from './Screens/NhanTin';
 import ThongKe from './Screens/ThongKe';
 import QuenMatKhau from './Screens/QuenMatKhau';
 import QuanLiTK from './Screens/QuanLiTK';
+import Home from './Screens/Home/home';
 const Drawer = createDrawerNavigator();
 
 function App() {
   return (
     <NavigationContainer>
       <Drawer.Navigator initialRouteName='QuanLiSP'>
-      <Drawer.Screen name="Quản lí sản phẩm" component={QuanLiSP} />
+      <Drawer.Screen name="Home" component={Home} options={{headerShown:false}} />
       <Drawer.Screen name="Quản lí dơn hàng" component={QuanLiDH} />
       <Drawer.Screen name="Quản lí blog" component={QuanLiBlog} />
       <Drawer.Screen name="Nhắn tin" component={NhanTin} />
